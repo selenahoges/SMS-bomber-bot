270facdfb1607dd44a066bb3b14018f092bd8b01
omadli <76625693+omadli@users.noreply.github.com>
2021-01-19 12:04:57 +0500

    Add files via upload

---
+++ Bot.py
@@ -0,0 +1,58 @@
+import telebot
+
+bot = telebot.TeleBot('1462354659:AAEr6Xm5ILWeqNNhGjyh9eLLSPZ-TM9DeLM')
+
+
+@bot.message_handler(commands=['help'])
+def start(message):
+    help = 'To start SMS Bomber type /bomber'
+    bot.send_message(message.chat.id, help)
+
+@bot.message_handler(commands=['cancel'])
+def cancel(message):
+    bot.send_message(message.from_user.id, 'Bomber cancelled. Type /bomber or /help')
+
+number = ''
+time = 0
+@bot.message_handler(content_types=['text'])
+def start(message):
+    if message.text == '/bomber':
+        bot.send_message(message.from_user.id, 'Input target number:')
+        bot.register_next_step_handler(message, get_number)
+    else:
+        bot.send_message(message.from_user.id, 'Type /bomber or /help')
+
+def get_number(message):
+    try:
+        if message.text != "/cancel":
+            global number
+            number = message.text
+            bot.send_message(message.from_user.id, 'Input attack time:')
+            bot.register_next_step_handler(message, get_time)
+        else:
+           bot.send_message(message.from_user.id, 'Bomber cancelled. Type /bomber or /help') 
+    except:
+        bot.send_message(message.from_user.id, "Error!")
+
+
+def get_time(message):
+    try:
+        if message.text != "/cancel":
+            global time
+            time = message.text
+            time_integer = int(time)
+            time = time_integer
+            main()
+        else:
+            bot.send_message(message.from_user.id, 'Bomber cancelled. Type /bomber or /help')
+    except:
+        bot.send_message(message.from_user.id, "Error!")
+
+
+def main():
+    threads = 4
+    from SMS.main import SMS_ATTACK
+    SMS_ATTACK(threads, time, number)
+
+
+bot.polling()
